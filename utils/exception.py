c47eac4e3525a69c42caff8cd25d49e67f97f3e0
dianhsu <xudian.cn@gmail.com>
2020-08-03 02:21:42 +0800

    修改了一下全局Exception抓取的匹配规则

--- utils/exception.py
+++ utils/exception.py
@@ -1,14 +1,13 @@
 from rest_framework.views import exception_handler
-from utils.response import msg
 from rest_framework.response import Response
+from .response import msg
 
 
 def custom_exception_handler(exc, context):
     # Call REST framework's default exception handler first,
     # to get the standard error response.
     response = exception_handler(exc, context)
-    res = Response()
-    if response is not None:
-        res.data = msg(err=response.data['detail'])
-        res.status_code = 200
-    return res
+    # Now add the HTTP status code to the response.
+    if response is not None and response.status_code in [400, 401, 403, 404]:
+        return Response(msg(err=response.data['detail']))
+    return response
