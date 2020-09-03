2d329462df061dd881e820261813c42750c6af39
dianhsu <xudian.cn@gmail.com>
2020-09-03 22:59:25 +0800

    add celery task test

--- submission/tasks.py
+++ submission/tasks.py
@@ -4,17 +4,21 @@ from celery import shared_task
 
 from submission.config import Verdict
 from submission.models import Submission
+from django.core.exceptions import ObjectDoesNotExist
 
 
 @shared_task
-def run(pk):
-    submission = Submission.objects.get(id=pk)
-    if platform.system() != 'Linux':
-        submission.verdict = Verdict.SYSTEM_ERROR
-        submission.additional_info = 'Linux platform required.'
-    else:
-        manifest = submission.problem.manifest
-        time_limit = submission.problem.time_limit
-        memory_limit = submission.problem.memory_limit
-        code = submission.code
-        print(manifest, time_limit, memory_limit, code)
+def run_submission_task(pk):
+    try:
+        submission = Submission.objects.get(id=pk)
+        if platform.system() != 'Linux':
+            submission.verdict = Verdict.SYSTEM_ERROR
+            submission.additional_info = 'Linux platform required.'
+        else:
+            manifest = submission.problem.manifest
+            time_limit = submission.problem.time_limit
+            memory_limit = submission.problem.memory_limit
+            code = submission.code
+            print(manifest, time_limit, memory_limit, code)
+    except ObjectDoesNotExist:
+        pass
