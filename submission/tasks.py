9ec3af5b19028ffdaf63d59f46333173a207904a
dianhsu <xudian.cn@gmail.com>
2020-09-07 18:44:50 +0800

    fix bugs

--- submission/tasks.py
+++ submission/tasks.py
@@ -5,7 +5,7 @@ from django.core.exceptions import ObjectDoesNotExist
 
 from submission.config import Verdict, Language
 from submission.models import Submission
-
+from ddl.settings import PROBLEM_TEST_CASES_DIR
 from ddlcw import Runner
 
 
@@ -21,11 +21,16 @@ def run_submission_task(pk):
             submission.time_spend = 1000
             submission.memory_spend = 3
             submission.save()
-            runner = Runner(submission.problem.manifest,
-                            submission.problem.time_limit,
-                            submission.problem.memory_limit,
-                            submission.code,
-                            Language.LANGUAGE_CONFIG[submission.lang])
-            print(runner.__dict__)
+            runner = Runner(
+                PROBLEM_TEST_CASES_DIR,
+                submission.problem.manifest,
+                submission.problem.time_limit,
+                submission.problem.memory_limit,
+                submission.code,
+                Language.LANGUAGE_CONFIG[submission.lang])
+            print('compile')
+            print(runner.compile())
+            print('runner')
+            print(runner.run())
     except ObjectDoesNotExist:
         pass
